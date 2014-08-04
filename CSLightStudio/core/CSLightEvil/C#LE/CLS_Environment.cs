61c514e569e7bde9947f884bb86d29ff7e0a97b8
lightszero <lightsever@hotmail.com>
2014-08-04 15:47:12 +0800

    readme 修改

--- CSLightStudio/core/CSLightEvil/C#LE/CLS_Environment.cs
+++ CSLightStudio/core/CSLightEvil/C#LE/CLS_Environment.cs
@@ -18,7 +18,7 @@ namespace CSLE
         {
             get
             {
-                return "0.36.1Alpha";
+                return "0.36.3Alpha";
             }
         }
         public CLS_Environment(ICLS_Logger logger)
