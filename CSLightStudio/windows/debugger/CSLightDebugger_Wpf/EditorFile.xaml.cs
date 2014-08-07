13642ab57360b149964590d45abcf4c544146c2b
lightszero <lightsever@hotmail.com>
2014-08-07 21:08:30 +0800

    21

---
+++ CSLightStudio/windows/debugger/CSLightDebugger_Wpf/EditorFile.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CSLightDebugger_Wpf
+{
+    /// <summary>
+    /// EditorFile.xaml 的交互逻辑
+    /// </summary>
+    public partial class EditorFile : Window
+    {
+        public EditorFile()
+        {
+            InitializeComponent();
+        }
+    }
+}
