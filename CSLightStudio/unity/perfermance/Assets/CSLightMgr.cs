376daf4ba292363f17b1166e15c1ba2162ee5c8a
lightszero <lightsever@hotmail.com>
2014-08-18 10:53:33 +0800

    个

---
+++ CSLightStudio/unity/perfermance/Assets/CSLightMgr.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Text;
+using UnityEngine;
+
+
+class Logger : CSLE.ICLS_Logger
+{
+    public void Log(string str)
+    {
+        Debug.Log(str);
+    }
+
+    public void Log_Error(string str)
+    {
+        Debug.LogError(str);
+    }
+
+    public void Log_Warn(string str)
+    {
+        Debug.LogWarning(str);
+    }
+}
+public class MYDebug:CSLE.RegHelper_Type
+{
+    public MYDebug():base(typeof(Debug),"Debug")
+{
+    function = new MyFunc();
+}
+    public class MyFunc:CSLE.RegHelper_TypeFunction
+    {
+        public MyFunc():base(typeof(Debug))
+        {
+
+        }
+        public override CSLE.CLS_Content.Value StaticCall(CSLE.CLS_Content environment, string function, IList<CSLE.CLS_Content.Value> _params)
+        {
+            if(function=="Log")
+            {
+                Debug.Log(_params[0].value as string);
+                return CSLE.CLS_Content.Value.Void;
+            }
+            return base.StaticCall(environment, function, _params);
+        }
+    }
+}
+public class MYGameObject : CSLE.RegHelper_Type
+{
+    public MYGameObject()
+        : base(typeof(GameObject), "GameObject")
+    {
+        function = new MyFunc();
+    }
+    public class MyFunc : CSLE.RegHelper_TypeFunction
+    {
+        public MyFunc()
+            : base(typeof(GameObject))
+        {
+
+        }
+        public override CSLE.CLS_Content.Value StaticCall(CSLE.CLS_Content environment, string function, IList<CSLE.CLS_Content.Value> _params)
+        {
+            switch(function)
+            {
+                case "Destory":
+                    GameObject.Destroy(_params[0].value as UnityEngine.Object);
+                    return CSLE.CLS_Content.Value.Void;
+                case "DestroyImmediate":
+                    GameObject.DestroyImmediate(_params[0].value as UnityEngine.Object);
+                    return CSLE.CLS_Content.Value.Void;
+                case "Find":
+                    {
+                        CSLE.CLS_Content.Value v =new CSLE.CLS_Content.Value();
+                        v.value=GameObject.Find(_params[0].value as string);
+                        v.type = typeof(GameObject);
+                        return v;
+                    }
+            }
+
+            
+            return base.StaticCall(environment, function, _params);
+        }
+        public override void MemberValueSet(CSLE.CLS_Content content, object object_this, string valuename, object value)
+        {
+            if (valuename == "name")
+            {
+                ((GameObject)object_this).name = value as string;
+                return;
+            }
+            base.MemberValueSet(content, object_this, valuename, value);
+        }
+        public override CSLE.CLS_Content.Value MemberValueGet(CSLE.CLS_Content environment, object object_this, string valuename)
+        {
+            if (valuename == "transform")
+            {
+                CSLE.CLS_Content.Value v = new CSLE.CLS_Content.Value();
+                v.value=                ((GameObject)object_this).transform;
+                v.type =typeof(Transform);
+
+                return v;
+            }
+            return base.MemberValueGet(environment, object_this, valuename);
+        }
+    }
+}
+public class MYTransform : CSLE.RegHelper_Type
+{
+    public MYTransform()
+        : base(typeof(Transform), "Transform")
+    {
+        function = new MyFunc();
+    }
+    public class MyFunc : CSLE.RegHelper_TypeFunction
+    {
+        public MyFunc()
+            : base(typeof(Transform))
+        {
+
+        }
+
+        public override void MemberValueSet(CSLE.CLS_Content content, object object_this, string valuename, object value)
+        {
+            switch(valuename)
+            {
+                case "localScale":
+                    ((Transform)object_this).localScale = (Vector3)value;
+                    return;
+                case "parent":
+                    ((Transform)object_this).parent = value as Transform;
+                return;
+            }
+
+            base.MemberValueSet(content, object_this, valuename, value);
+        }
+    }
+}
+
+public class MYVector3 : CSLE.RegHelper_Type
+{
+    public MYVector3()
+        : base(typeof(Vector3), "Vector3")
+    {
+        function = new MyFunc();
+    }
+    public class MyFunc : CSLE.RegHelper_TypeFunction
+    {
+        public MyFunc()
+            : base(typeof(Vector3))
+        {
+
+        }
+        public override CSLE.CLS_Content.Value StaticValueGet(CSLE.CLS_Content environment, string valuename)
+        {
+            if(valuename=="one")
+            {
+                CSLE.CLS_Content.Value v = new CSLE.CLS_Content.Value();
+                v.value = Vector3.one;
+                v.type = typeof(Vector3);
+                return v;
+            }
+            return base.StaticValueGet(environment, valuename);
+        }
+    }
+}
+    class CSLightMgr
+    {
+        public static CSLE.CLS_Environment env = null;
+        //public static LuaInterface.LuaState luser = null;
+        static Dictionary<int, CSLE.ICLS_Expression> expcache = new Dictionary<int, CSLE.ICLS_Expression>();
+        public static void InitCSLight()
+        {
+            env = new CSLE.CLS_Environment(new Logger());
+            //env.RegType(new CSLE.RegHelper_Type(typeof(GameObject)));
+            env.RegType(new MYGameObject());
+            //env.RegType(new CSLE.RegHelper_Type(typeof(Debug)));
+            env.RegType(new MYDebug());      
+            //env.RegType(new CSLE.RegHelper_Type(typeof(Transform)));
+            env.RegType(new MYTransform());
+            env.RegType(new CSLE.RegHelper_Type(typeof(Camera)));
+            env.RegType(new CSLE.RegHelper_Type(typeof(Vector3)));
+        }
+        public static void InitLua()
+        {
+//            luser = new LuaInterface.LuaState();
+//            string initlua =
+//            @"  luanet.load_assembly('UnityEngine')
+//			    luanet.load_assembly('Assembly-CSharp')
+//                GameObject = luanet.import_type('UnityEngine.GameObject')
+//                Debug = luanet.import_type('UnityEngine.Debug')
+//			    Camera = luanet.import_type('UnityEngine.Camera')
+//			    Vector3 = luanet.import_type('UnityEngine.Vector3');
+//			    Resources = luanet.import_type('UnityEngine.Resources')
+//";
+//            luser.DoString(initlua);
+        }
+        public static void LuaDoString(string code)
+        {
+            //luser.DoString(code);
+        }
+        public static void CSLightDoString(string code)
+        {
+            var c = new CSLE.CLS_Content(env,false);
+            int hash=code.GetHashCode();
+            if(expcache.ContainsKey(hash))
+            {
+                env.Expr_Execute(expcache[hash],c);
+                return;
+            }
+            var t = env.ParserToken(code);
+            var e = env.Expr_CompilerToken(t);
+            expcache[hash] = e;
+            env.Expr_Execute(e,c);
+        }
+    }
+
---
+++ CSLightStudio/unity/perfermance/Assets/TestCool.cs
@@ -0,0 +1,178 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Collections.Generic;
+
+public class TestCool : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        CSLightMgr.InitCSLight();
+        CSLightMgr.InitLua();
+        //Pre 保持平等对抗
+        Test01(1);
+        Test02(1);
+        Test03(1);
+
+        //开始对抗
+        result.Add(Test02(300));
+        result.Add( Test01(3000));
+        result.Add(Test03(1000));
+    }
+    List<TestResult> result = new List<TestResult>();
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnGUI()
+    {
+        for (int y = 0; y < result.Count; y++)
+        {
+            GUI.Label(new Rect(0, y * 50, Screen.width, 50), result[y].ToString());
+        }
+    }
+    class TestResult
+    {
+        public TestResult(string name)
+        {
+            this.name = name;
+        }
+        public string name;
+        public double baseTime;
+        public double T1Time;
+        public double T2Time;
+        public override string ToString()
+        {
+            return name + " B:" + baseTime + "|T1:" + T1Time+"(X"+(T1Time/baseTime) + ")|T2:" + T2Time+"(X"+(T2Time/baseTime)+")";
+        }
+    }
+    TestResult Test01(int count)
+    {
+        TestResult r = new TestResult("New&Destroy 1");
+
+        DateTime t0 = DateTime.Now;
+
+        for (int i = 0; i < count; i++)
+        {
+            GameObject o = new GameObject();
+            o.name = "Unity";
+            GameObject.DestroyImmediate(o);
+        }
+        DateTime t1 = DateTime.Now;
+
+        for (int i = 0; i < count; i++)
+        {
+            //ulua执行这个代码都不正常
+            //CSLightMgr.InitLua();
+            CSLightMgr.LuaDoString(
+            @"  
+            local o = GameObject();
+            o.name='lua';
+            GameObject.Destroy(o);
+            "
+                );
+        }
+        DateTime t2 = DateTime.Now;
+
+        for (int i = 0; i < count; i++)
+        {
+            CSLightMgr.CSLightDoString(
+            @"
+            GameObject o =new GameObject();
+            o.name=""C#LE"";
+            GameObject.DestroyImmediate(o);"
+            );
+        }
+        DateTime t3 = DateTime.Now;
+        r.baseTime = (t1 - t0).TotalSeconds;
+        r.T1Time = (t2 - t1).TotalSeconds;
+        r.T2Time = (t3 - t2).TotalSeconds;
+        return r;
+    }
+    TestResult Test02(int count)
+    {
+        TestResult r = new TestResult("Log Test");
+
+        DateTime t0 = DateTime.Now;
+
+        for (int i = 0; i < count; i++)
+        {
+            Debug.Log("Unity Log");
+        }
+        DateTime t1 = DateTime.Now;
+
+        for (int i = 0; i < count; i++)
+        {
+            CSLightMgr.LuaDoString("Debug.Log(\"luser log\")");
+        }
+        DateTime t2 = DateTime.Now;
+
+        for (int i = 0; i < count; i++)
+        {
+            CSLightMgr.CSLightDoString("Debug.Log(\"C#Light log\");");
+        }
+        DateTime t3 = DateTime.Now;
+        r.baseTime = (t1 - t0).TotalSeconds;
+        r.T1Time = (t2 - t1).TotalSeconds;
+        r.T2Time = (t3 - t2).TotalSeconds;
+        return r;
+    }
+
+    TestResult Test03(int count)
+    {
+        TestResult r = new TestResult("New&Destroy 10");
+
+        DateTime t0 = DateTime.Now;
+
+        for (int i = 0; i < count; i++)
+        {
+            for(int j=0;j<10;j++)
+            {
+                GameObject go = new GameObject();
+                GameObject camera = GameObject.Find("Camera");
+                go.transform.parent = camera.transform;
+                go.transform.localScale = Vector3.one;
+                GameObject.DestroyImmediate(go);
+            }
+        }
+        DateTime t1 = DateTime.Now;
+
+        for (int i = 0; i < count; i++)
+        {
+            CSLightMgr.LuaDoString(
+            @"for i=1,10 do  
+                local go = GameObject()
+			    local camera = GameObject.Find('Camera');
+			    go.transform.parent = camera.transform;
+			    go.transform.localScale = Vector3.one;
+                GameObject.DestroyImmediate(go)
+            end"
+                );
+        }
+        DateTime t2 = DateTime.Now;
+
+        for (int i = 0; i < count; i++)
+        {
+            CSLightMgr.CSLightDoString(
+            @"
+            for(int j=0;j<10;j++)
+            {
+                GameObject go = new GameObject();
+                GameObject camera = GameObject.Find(""Camera"");
+                go.transform.parent = camera.transform;
+                go.transform.localScale = Vector3.one;
+                GameObject.DestroyImmediate(go);
+            }"
+            );
+        }
+        DateTime t3 = DateTime.Now;
+        r.baseTime = (t1 - t0).TotalSeconds;
+        r.T1Time = (t2 - t1).TotalSeconds;
+        r.T2Time = (t3 - t2).TotalSeconds;
+        return r;
+    }
+
+}
