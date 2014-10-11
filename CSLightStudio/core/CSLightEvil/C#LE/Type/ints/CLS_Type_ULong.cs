f75c9128b65f412ed6b2a4beafffbb24cebeb3fb
Graphic <codinglikecrazy@gmail.com>
2014-10-11 10:38:27 +0800

    ulong.

---
+++ CSLightStudio/core/CSLightEvil/C#LE/Type/ints/CLS_Type_ULong.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSLE
+{
+    class CLS_Type_ULong : RegHelper_Type
+    {
+        public CLS_Type_ULong()
+            : base(typeof(ulong), "ulong")
+        {
+
+        }
+
+        public override object ConvertTo(CLS_Content env, object src, CLType targetType)
+        {
+            bool convertSuccess = false;
+            object convertedObject = NumericTypeUtils.TryConvertTo<ulong>(src, targetType, out convertSuccess);
+            if (convertSuccess) {
+                return convertedObject;
+            }
+
+            return base.ConvertTo(env, src, targetType);
+        }
+
+        public override object Math2Value(CLS_Content env, char code, object left, CLS_Content.Value right, out CLType returntype)
+        {
+            bool math2ValueSuccess = false;
+            object value = NumericTypeUtils.Math2Value<ulong>(code, left, right, out returntype, out math2ValueSuccess);
+            if (math2ValueSuccess) {
+                return value;
+            }
+
+            return base.Math2Value(env, code, left, right, out returntype);
+        }
+
+        public override bool MathLogic(CLS_Content env, logictoken code, object left, CLS_Content.Value right)
+        {
+            bool mathLogicSuccess = false;
+            bool value = NumericTypeUtils.MathLogic<ulong>(code, left, right, out mathLogicSuccess);
+            if (mathLogicSuccess) {
+                return value;
+            }
+
+            return base.MathLogic(env, code, left, right);
+        }
+
+        public override object DefValue
+        {
+            get { return (ulong)0; }
+        }
+    }
+}
